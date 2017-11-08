edba4615567f6b25ce97c5218a25c1ce807ab9ff
Greg Weiss <gergusa04@gmail.com>
2017-11-08 09:39:14 -0500

    added function

--- BlankSpaces/ViewController.swift
+++ BlankSpaces/ViewController.swift
@@ -38,6 +38,10 @@ class ViewController: UIViewController {
         aNum = 9
         print(aNum)
     }
+    
+    func doSomethingHere() {
+        print("hello again my num is \(myNum)")
+    }
 
 }
 
