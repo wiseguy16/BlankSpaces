0864c1659027565db622b26e1dffb85b47a056b9
Greg Weiss <gergusa04@gmail.com>
2017-11-08 09:10:05 -0500

    all new changes made

--- BlankSpaces/ViewController.swift
+++ BlankSpaces/ViewController.swift
@@ -10,13 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
     var aName: String?
+    let myNum = 8
+    var aNum: Int?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        aNum = myNum
         aName = "Hello There"
         guard let name = aName else { return }
         print(name)
-        
         // This project should check for unneaded spaces
         // This is first push to remote
         // Do any additional setup after loading the view, typically from a nib.
