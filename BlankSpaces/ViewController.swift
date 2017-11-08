d74e6a1d1ce59ad972d3e2125625ed7fe1ebc8dc
Greg Weiss <gergusa04@gmail.com>
2017-11-08 09:12:23 -0500

    adding spaces

--- BlankSpaces/ViewController.swift
+++ BlankSpaces/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         aNum = myNum
         aName = "Hello There"
         guard let name = aName else { return }
